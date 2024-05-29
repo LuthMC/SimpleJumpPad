a14116032637456670b8f9a70eeb6472384344bc
LuthMC <134492162+LuthMC@users.noreply.github.com>
2024-05-29 11:34:43 +0700

    Update #1

--- src/Luthfi/SimpleJumpPad/Main.php
+++ src/Luthfi/SimpleJumpPad/Main.php
@@ -8,7 +8,8 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\math\Vector3;
-use pocketmine\block\BlockTypeIds;
+use pocketmine\block\BlockLegacyIds;
+use pocketmine\block\Block;
 use pocketmine\utils\Config;
 
 class Main extends PluginBase implements Listener {
@@ -27,9 +28,9 @@ class Main extends PluginBase implements Listener {
 
     public function onPlayerMove(PlayerMoveEvent $event): void {
         $player = $event->getPlayer();
-        $block = $player->getWorld()->getBlock($player->getPosition()->subtract(0, 1, 0));
+        $block = $player->getWorld()->getBlock($player->getPosition()->floor()->down());
         
-        if ($block->getTypeId() === BlockTypeIds::LIGHT_WEIGHTED_PRESSURE_PLATE) {
+        if ($block->getId() === BlockLegacyIds::LIGHT_WEIGHTED_PRESSURE_PLATE) {
             $player->setMotion(new Vector3(0, $this->jumpPower, 0));
         }
     }
